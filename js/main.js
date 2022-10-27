96af3183a6964c9e491499e0aa9f4f19c0f3aa2d
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-10-27 15:07:53 +0200

    adds changes

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
 const age = parseInt(prompt("Inserisci l'età del passeggero"));
 const percorso = parseInt(prompt("Inserisci il numero di chilometri da percorrere"));
+
+const pricePerKm = 0.21;
+const distance = percorso;
+
+let totalPrice = (pricePerKm * distance);
+console.log(totalPrice);
+
+if (age < 18) {
+    console.log("Il passegero ha uno sconto del 20% sul biglietto");
+    
+} 
+
+if (age > 65){
+    console.log("Il passeggero ha uno sconto del 40% sul biglietto");
+}
+
