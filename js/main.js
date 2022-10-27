a36bde36bb34968168623027371b180860ed170c
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-10-28 00:02:52 +0200

    adds fixed code

--- js/main.js
+++ js/main.js
@@ -5,20 +5,18 @@ const pricePerKm = 0.21;
 const distance = percorso;
 
 const totalPrice = (pricePerKm * distance);
-const numDecimale = totalPrice;
+const numPriceDiscount20 = (totalPrice * 0.8 );
+const numPriceDiscount40 = (totalPrice * 0.6);
 
 
 if (age < 18) {
     console.log("Il passegero ha uno sconto del 20% sul biglietto");
-    console.log(numDecimale.toFixed(2)); 
-    console.log(totalPrice * 0.8);
+    console.log(numPriceDiscount20.toFixed(2));
 } else if (age > 18 && age < 65) {
-    console.log(numDecimale.toFixed(2));
+    console.log(totalPrice.toFixed(2));
 
 } else if (age > 65){
     console.log("Il passeggero ha uno sconto del 40% sul biglietto");
-    console.log(numDecimale.toFixed(2));
-    console.log(totalPrice * 0.6);
-    
+    console.log(numPriceDiscount40.toFixed(2));
 }
 
