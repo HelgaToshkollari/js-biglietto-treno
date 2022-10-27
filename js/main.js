fcf982434acad6e913fecf29cecf67377d49b7c2
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-10-27 23:21:08 +0200

    adds changes

--- js/main.js
+++ js/main.js
@@ -7,19 +7,18 @@ const distance = percorso;
 const totalPrice = (pricePerKm * distance);
 const numDecimale = totalPrice;
 
-console.log(totalPrice);
-console.log(numDecimale.toFixed(2));
-
-
 
 if (age < 18) {
     console.log("Il passegero ha uno sconto del 20% sul biglietto");
+    console.log(numDecimale.toFixed(2)); 
     console.log(totalPrice * 0.8);
-    
-} 
+} else if (age > 18 && age < 65) {
+    console.log(numDecimale.toFixed(2));
 
-if (age > 65){
+} else if (age > 65){
     console.log("Il passeggero ha uno sconto del 40% sul biglietto");
+    console.log(numDecimale.toFixed(2));
     console.log(totalPrice * 0.6);
+    
 }
 
