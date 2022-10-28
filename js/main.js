3747b326d6f6f7f26a568952d40723cb9836a78c
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-10-28 10:53:53 +0200

    adds changes

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ if (age < 18) {
     console.log("Il passegero ha uno sconto del 20% sul biglietto");
     console.log(" Costo biglietto € " + numPriceDiscount20.toFixed(2));
 } else if (age >= 18 && age < 65) {
-    console.log("Costo biglietto : " + "€ " + totalPrice.toFixed(2));
+    console.log(" Costo biglietto € " + totalPrice.toFixed(2));
 
 } else if (age > 65){
     console.log("Il passeggero ha uno sconto del 40% sul biglietto");
